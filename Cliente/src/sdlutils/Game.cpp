3af29a5335e46b128e8581284b009dcf8b8fd1d8
Javier <jmeitin@ucm.es>
2022-06-03 16:18:52 +0200

    tabulador

--- Cliente/src/sdlutils/Game.cpp
+++ Cliente/src/sdlutils/Game.cpp
@@ -6,7 +6,6 @@ Client::Client(const char* ip, const char* port, const char * n): socket(ip,port
 	nick = n;
 
 	std::cout << nick<<std::endl;
-
 	
 }
 
@@ -15,19 +14,18 @@ Client::~Client(){
 	if(player2 != nullptr)delete player2;
 	freeDeadBullets();
 	while (!MyBullets.empty()) {
-       	 	delete MyBullets.back();
-       		MyBullets.pop_back();
+		delete MyBullets.back();
+		MyBullets.pop_back();
    	} 
-	   while (!EnemyBullets.empty()) {
-       	 	delete EnemyBullets.back();
-       		EnemyBullets.pop_back();
+	while (!EnemyBullets.empty()) {
+		delete EnemyBullets.back();
+		EnemyBullets.pop_back();
    	} 
 }
 
 
 void Client::login()
-{
-	
+{	
     LogMessage em(nick);
     em.type = Message::MessageType::LOGIN;
 	
@@ -86,7 +84,7 @@ void Client::net_thread()
 void Client::startGame(){
     // Initialise the SDLGame singleton
 	try{
-	SDLUtils::init("SDLGame Demo!", WIDTH, HEIGHT,
+		SDLUtils::init("SDLGame Demo!", WIDTH, HEIGHT,
 			"../resources/config/sdlutilsdemo.resources.json");
 	}
 	catch (const std::string &e) { // catch exceptions thrown as strings
@@ -102,16 +100,19 @@ void Client::startGame(){
      sdl = SDLUtils::instance();
 	// std::cout << (sdl == nullptr) << std::endl;
     // //show the cursor
-	 sdl->showCursor();
+	sdl->showCursor();
 
 	// // store the 'renderer' in a local variable, just for convenience
-	 renderer = sdl->renderer();
+	renderer = sdl->renderer();
 
 	// // we can take textures from the predefined ones, and we can create a custom one as well
-	// // player = new Player(this, &sdl->images().at("fighter"),posiciones[MyPlayerID].first,posiciones[MyPlayerID].second, SPEED, WIDTH, HEIGHT);
-	// // int otherID = (MyPlayerID + 1) % 2	;
-	// // player2 = new Player(this, &sdl->images().at("fighter"),posiciones[otherID].first,posiciones[otherID].second, SPEED, WIDTH, HEIGHT);
-	  ih = InputHandler::instance();
+	// player = new Player(this, &sdl->images().at("fighter"),posiciones[MyPlayerID].first,posiciones[MyPlayerID].second, SPEED, WIDTH, HEIGHT);
+	// int otherID = (MyPlayerID + 1) % 2	;
+	// player2 = new Player(this, &sdl->images().at("fighter"),posiciones[otherID].first,posiciones[otherID].second, SPEED, WIDTH, HEIGHT);
+	
+	ih = InputHandler::instance();
+
+	//playing = true;
 
     
 }
@@ -120,48 +121,50 @@ void Client::game_thread(){
     while (!exit_) {
 		if(playing){
 			
-		// Uint32 startTime = sdl->currRealTime();
-		
-		// // update the event handler
-		ih->refresh();
-			// clear screen
-		sdl->clearRenderer();
+			//Uint32 startTime = sdl->currRealTime();
+			
+			// // update the event handler
+			ih->refresh();
+				// clear screen
+			sdl->clearRenderer();
 
-		// exit when any key is down
-		// if (ih->keyDownEvent() )
-		// {
-		// 	if(ih->isKeyDown(SDLK_q))
-		// 	exit_ = true;
-		// }
+			//exit when any key is down
+			// if (ih->keyDownEvent() )
+			// {
+			// 	if(ih->isKeyDown(SDLK_q))
+			// 	exit_ = true;
+			// }
 
-		
-	// 	if(player->update()){
-	// 		Object posMsg = Object(MyPlayerID, player->GetPosition().first, player->GetPosition().second, player->getRot());
-	// 		socket.send(posMsg, socket);
+			
+			// 	if(player->update()){
+			// 		Object posMsg = Object(MyPlayerID, player->GetPosition().first, player->GetPosition().second, player->getRot());
+			// 		socket.send(posMsg, socket);
 
-	// 	}
+			// 	}
 
-	// 	checkCollision();
-		
-	// updateAllBullets();
-	// freeDeadBullets();
-	// 	//RENDER---------------------------------------------------
-	// 	for(Bala* bullet : MyBullets) bullet->render();
-	// 	for(Bala* bullet : EnemyBullets) bullet->render();
+			// 	checkCollision();
+				
+			// updateAllBullets();
+			// freeDeadBullets();
+			// 	//RENDER---------------------------------------------------
+			// 	for(Bala* bullet : MyBullets) bullet->render();
+			// 	for(Bala* bullet : EnemyBullets) bullet->render();
 
-	// 	player->render();
-	// 	player2->render();
-		
-		
-	// 	// present new frame
-	// 	sdl->presentRenderer();
+			// 	player->render();
+			// 	player2->render();
+				
+				
+			// 	// present new frame
+			// 	sdl->presentRenderer();
 
-	// 	Uint32 frameTime = sdl->currRealTime() - startTime;
+			// Uint32 frameTime = sdl->currRealTime() - startTime;
 
-	// 	if (frameTime < 20)
-	// 		SDL_Delay(20 - frameTime);
-		 }
-	 }
+			// if (frameTime < 20)
+			// 	SDL_Delay(20 - frameTime);
+
+		} //fin if playing
+	} //fin while
+
 	// stop the music
 	//Music::haltMusic();
 }
@@ -178,7 +181,6 @@ void Client::freeDeadBullets(){
 		delete EnemyDeadBullets.back();
 		EnemyDeadBullets.pop_back();
 	}
-
 }
 
 void Client::updateAllBullets(){
@@ -197,8 +199,7 @@ void Client::updateAllBullets(){
 
 		if (currentPos.first >= WIDTH || currentPos.first <= 0||currentPos.second  >= HEIGHT||currentPos.second <= 0)  //SI SE MUERE
 			EnemyDeadBullets.push_back((*bullet));
-	}
-	
+	}	
 }
 
 void Client::crearBala(pair<int,int> currentPos, double rot){
@@ -218,23 +219,24 @@ void Client::crearBalaEnemiga(pair<int,int> pos, double rot){
 
 void Client::checkCollision(){
 	for (auto bullet =EnemyBullets.begin(); bullet != EnemyBullets.end(); bullet++){
-        Bala* b = (*bullet);		
-			if (Collisions::collidesWithRotation(
-			b->getPosition2(), b->getW(), b->getH(), b->getRot(),
-			player->getPosition2(), player->getW(), player->getH(), player->getRot())) {
-				MyDeadBullets.push_back((*bullet));
-				exit_ = true;
+        Bala* b = (*bullet);	
 
-				PlayerMsg msg = PlayerMsg(MyPlayerID);
-				msg.type = Message::MessageType::PlAYERKILLED;
-				socket.send(msg, socket);
-				//mensaje de que he perdido -----------------------------
-				std::cout<< "ganó jugador B";
-			}
-			
+		if (Collisions::collidesWithRotation(
+		b->getPosition2(), b->getW(), b->getH(), b->getRot(),
+		player->getPosition2(), player->getW(), player->getH(), player->getRot())) {
+			MyDeadBullets.push_back((*bullet));
+			exit_ = true;
+
+			PlayerMsg msg = PlayerMsg(MyPlayerID);
+			msg.type = Message::MessageType::PlAYERKILLED;
+			socket.send(msg, socket);
+			//mensaje de que he perdido -----------------------------
+			std::cout<< "ganó jugador B";
 		}
 			
-    }
+	}
+		
+}
 	
 
 
